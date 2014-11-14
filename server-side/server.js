b1a9562c821f9f1740768505c76c68418b35ba2e
sean-sq-chen <sean.sq.chen@gmail.com>
2014-11-13 16:52:39 -0800

    Using new API of expressjs and adding color to console outup.

--- server-side/server.js
+++ server-side/server.js
@@ -1,9 +1,6 @@
 var express = require('express'),
     server = express(),
     port = parseInt(process.argv[2]) || 3000;
-    
-server.configure(function () {
-    this.use(express.static('../client-side/'));
-}).listen(port);
 
-console.log('HTTP Server starts listening on %d ...', port);
+server.use(express.static('../client-side/')).listen(port);
+console.log('\x1b[32mHTTP Server starts listening on %d ...\x1b[0m', port);
