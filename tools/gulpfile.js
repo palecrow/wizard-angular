d87b0a195943096aeb13c944e4a1fc557c676c91
Shaoquan Chen <sean.chen2@hp.com>
2015-01-06 11:53:44 -0800

    start using gulp
    - Add these tasks:
      `lint`: JS lint.
      `sass`: compiling scss files to css files.
      `clean`: clean generated css files.
      `watch`: watching JS and scss file change and to related tasks.
    
    - Conver CSS files to scss files.

---
+++ tools/gulpfile.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var del       = require('del');
+var gulp      = require('gulp');
+var jshint    = require('gulp-jshint');
+var sass      = require('gulp-sass');
+
+var root = '../';
+
+var paths = {
+  root: root,
+  client: root + 'client-side/',
+  server: root + 'server-side/'
+};
+
+gulp.task('lint', function () {
+  return gulp.src([paths.client + 'app/**/*.js', paths.client + 'framework/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src([paths.client + '**/*.scss'], {base: './'})
+    .pipe(sass())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.client + '**/*.js', ['lint']);
+    gulp.watch(paths.client + '**/*.scss', ['sass']);
+});
+
+gulp.task('clean', function (callback) {
+  del([paths.client + '**/*.css', '!' + paths.client + 'lib/**/*.css'], {
+    force: true
+  }, callback);
+});
+
+gulp.task('default', ['lint', 'sass', 'watch']);
